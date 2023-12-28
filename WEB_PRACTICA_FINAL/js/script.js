b29c2e05108301d629cb7cdad96b0f2fa6138706
anatorrico <anatorricotirado@gmail.com>
2023-12-28 21:12:28 +0100

    nav responsive 2
    vamos empate

--- WEB_PRACTICA_FINAL/js/script.js
+++ WEB_PRACTICA_FINAL/js/script.js
@@ -1,39 +1,5 @@
 $(document).ready(function(){
 
-    // $( "#menubtn" ).click(function() {
-    //     $( "#links_menu" ).slideToggle( "slow", function() {
-    //         $("#links_menu").css(
-    //             {
-    //                 "background-color": "#F4F4ED", 
-    //                 "text-align": "left",
-    //                 "width": "80%",
-    //                 "position": "absolute",
-    //                 "z-index": "1000",
-    //                 "top": "6em",
-    //                 "padding": "1em 4em",
 
-                
-    //             }
-    //         );
-    //     });
-    // });
-
-    $( "#menubtn" ).click(function() {
-        var x = $("#mymenu");
-        if (x.className === "menu") {
-          x.className += " responsive";
-        } else {
-          x.className = "menu";
-        }
-    });
-
-    // function myFunction() {
-    //     var x = document.getElementById("myTopnav");
-    //     if (x.className === "topnav") {
-    //       x.className += " responsive";
-    //     } else {
-    //       x.className = "topnav";
-    //     }
-    //   }
 
 });
