a64fb4ee144c2f8d827007ebed54c4935e0127e6
anatorrico <anatorricotirado@gmail.com>
2024-01-03 20:41:57 +0100

    community gallery

--- WEB_PRACTICA_FINAL/js/script.js
+++ WEB_PRACTICA_FINAL/js/script.js
@@ -21,7 +21,33 @@ $(document).ready(function(){
             $(".section_title > img.LED_flecha").attr('src', 'media/img/flecha.png');
         }
     );
-
     
+    // $( '.horizontal-scroll' ).horizontalScroll({
+    //     containerHeight: "300vh",
+    //     paddingLeft: "3em" ,
+        
+    //     function(){
+    //         $("div#gallery_community.horizontal-scroll").css(
+    //             'margin-top', '0px');
+    //     }
+    // });
+    
+     
 
 });
+
+let items = document.querySelectorAll('.carousel .carousel-item')
+
+items.forEach((el) => {
+    const minPerSlide = 4
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+        	next = items[0]
+      	}
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
