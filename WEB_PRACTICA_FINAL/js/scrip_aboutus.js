128122ef5dc45d64c1cfa5e9b966b5008f8b9ffb
alazne5 <alaznegoni5@gmail.com>
2024-01-03 20:21:34 +0100

    menu+flechas

--- WEB_PRACTICA_FINAL/js/scrip_aboutus.js
+++ WEB_PRACTICA_FINAL/js/scrip_aboutus.js
@@ -14,3 +14,29 @@ $(document).ready(function(){
       }
     });
   });
+
+  $(document).ready(function(){
+
+    $(".hamburger").click(function(){
+    $(".toggle_menu").toggle();
+    });
+    
+    $("#logo").hover(
+    function(){
+    $("#logo > img").attr('src', 'media/img/logo horizontal-LED.png');
+    },
+    function(){
+    $("#logo > img").attr('src', 'media/img/Logo Horizontal.png');
+    }
+    );
+    
+    });
+
+    $(".section_title").hover(
+      function(){
+      $(".section_title > img.LED_flecha").attr('src', 'media/img/flecha_LED.png');
+      },
+      function(){
+      $(".section_title > img.LED_flecha").attr('src', 'media/img/flecha_LED.png');
+      }
+      );
