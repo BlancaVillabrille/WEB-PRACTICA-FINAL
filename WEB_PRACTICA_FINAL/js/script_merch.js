505aded8a9d1cef877639d26fa5cd5220b8c4157
BlancaVillabrille <blancavsp03@gmail.com>
2024-01-04 15:04:37 +0100

    footer menu flecha

--- WEB_PRACTICA_FINAL/js/script_merch.js
+++ WEB_PRACTICA_FINAL/js/script_merch.js
@@ -17,7 +17,30 @@
 
 
 
+$(document).ready(function(){
 
+    $(".hamburger").click(function(){
+        $(".toggle_menu").toggle();
+        });
+        
+        $("#logo").hover(
+        function(){
+        $("#logo > img").attr('src', 'media/img/logo horizontal-LED.png');
+        },
+        function(){
+        $("#logo > img").attr('src', 'media/img/Logo Horizontal.png');
+        }
+    );
+
+    $(".section_title").hover(
+        function(){
+            $(".section_title > img").attr('src', 'media/img/MERCH/flecha_LED.png');
+        },
+        function(){
+            $(".section_title > img").attr('src', 'media/img/flecha.png');
+        }
+    );
+});
 
 
 var modal2 = document.getElementsByClassName('modal-2');
