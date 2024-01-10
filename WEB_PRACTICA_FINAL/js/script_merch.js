a0bfb38b8bb8df1550b6be95246e0d7e679e86c4
BlancaVillabrille <blancavsp03@gmail.com>
2024-01-11 00:53:33 +0100

    ventana modal funciona

--- WEB_PRACTICA_FINAL/js/script_merch.js
+++ WEB_PRACTICA_FINAL/js/script_merch.js
@@ -55,14 +55,47 @@ $(document).ready(function(){
 
 
     // // VENTANA MODAL
-    $(".trigger1").click(function(){
-        $('.ventana1').css('visibility','visible');
-        $('.ventana1').css('display','flex');
-        console.log("Hello");
+    $("#trigger1, #trigger2").click(function() {
+        $("#ventana1").css("visibility","visible");
+        $("#ventana1").css("display","flex");
+    });
+    $("#trigger2").click(function() {
+        $("#ventana2").css("visibility","visible");
+        $("#ventana2").css("display","flex");
+    });
+    $("#trigger3").click(function() {
+        $("#ventana3").css("visibility","visible");
+        $("#ventana3").css("display","flex");
+    });
+    $("#trigger4").click(function() {
+        $("#ventana4").css("visibility","visible");
+        $("#ventana4").css("display","flex");
+    });
+    $("#trigger5").click(function() {
+        $("#ventana5").css("visibility","visible");
+        $("#ventana5").css("display","flex");
+    });
+    $("#trigger6").click(function() {
+        $("#ventana6").css("visibility","visible");
+        $("#ventana6").css("display","flex");
+    });
+    $("#trigge7").click(function() {
+        $("#ventana7").css("visibility","visible");
+        $("#ventana7").css("display","flex");
+    });
+    $("#trigger8").click(function() {
+        $("#ventana8").css("visibility","visible");
+        $("#ventana8").css("display","flex");
+    });
+    $(".btn-close").click(function() {
+        $("#ventana1, #ventana2, #ventana3, #ventana4, #ventana5, #ventana6, #ventana7, #ventana8 ").css("visibility","hidden");
+        $("#ventana1, #ventana2, #ventana3, #ventana4, #ventana5, #ventana6, #ventana7, #ventana8").css("display","block");
+    });
+    $(".btn-comprar-1").click(function() {
+        $("#ventana1, #ventana2, #ventana3, #ventana4, #ventana5, #ventana6, #ventana7, #ventana8").css("visibility","hidden");
+        $("#ventana1, #ventana2, #ventana3, #ventana4, #ventana5, #ventana6, #ventana7, #ventana8").css("display","block");
     });
 
-
-    
     // BOTONES IMAGENES
     // BOTONES TALLA
     var buttons = $(".btn-talla");
