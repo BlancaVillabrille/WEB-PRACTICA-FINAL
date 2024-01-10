8ee9700e58ff8527d63905387631eafaadbf65b2
informaticaesne <informatica@esne.es>
2024-01-10 20:36:26 +0100

    modal sin bootstrap

--- WEB_PRACTICA_FINAL/js/script_merch.js
+++ WEB_PRACTICA_FINAL/js/script_merch.js
@@ -55,66 +55,16 @@ $(document).ready(function(){
 
 
     // // VENTANA MODAL
-    
-    // // TITULO VENTANA
-    // var exampleModal = document.getElementsByClassName('modal-prueba')
-    // // if (exampleModal) {
-    //     var shorts = document.getElementById("modal-title").textContent;
-    //     shorts = "Shorts";
-    //     exampleModal.addEventListener('show.bs.modal', shorts)
-    //     $(".galeria > .btn ").click(function(){
-    //         // Button that triggered the modal
-    //         // var button = $(".trigger") ;
-    //         // Extract info from data-bs-* attributes
-    //         // var recipient = $(".trigger").val($(".trigger").attr("name"));
-    //         // If necessary, you could initiate an Ajax request here
-    //         // and then do the updating in a callback.
-
-    //         // Update the modal's content.
-    //         // var modalTitle =  $(".modal-title");
-    //         // var modalBodyInput = exampleModal.querySelector('.modal-body input');
-    //             // if (recipient.textContent = "Shorts"){
-    //             //     modalTitle.textContent = "Shorts"
-    //             // }
-            
-    //         document.getElementById("modal-title").textContent = "Shorts";
-    //         // modalTitle.textContent = `New message to ${recipient}`;
-    //         // modalBodyInput.value = recipient
-    //         console.log("Hello");
-        
-    //     // })
-    // // }
-
-
-
-    const exampleModal = document.getElementById('exampleModal')
-    if (exampleModal) {
-      exampleModal.addEventListener('show.bs.modal', event => {
-        // Button that triggered the modal
-        const button = event.relatedTarget
-        // Extract info from data-bs-* attributes
-        const recipient = button.getAttribute('data-bs-whatever')
-        // If necessary, you could initiate an Ajax request here
-        // and then do the updating in a callback.
-    
-        // Update the modal's content.
-        const modalTitle = exampleModal.querySelector('.modal-title')
-        const modalBodyInput = exampleModal.querySelector('.modal-body input')
-    
-        modalTitle.textContent = `New message to ${recipient}`
-        modalBodyInput.value = recipient
-      })
-    }
-
-
+    $(".trigger1").click(function(){
+        $('.ventana1').css('visibility','visible');
+        $('.ventana1').css('display','flex');
+        console.log("Hello");
+    });
 
 
     
     // BOTONES IMAGENES
     // BOTONES TALLA
-
-
-
     var buttons = $(".btn-talla");
 
     buttons.click(function() {
@@ -126,27 +76,6 @@ $(document).ready(function(){
         $(".btn-talla").removeClass("pink"); // Elimina la clase pink de todos los botones
     });
 
-
-
-
-
-
-    // var bgcolorxs = $('.btn-talla-xs').css('background-color');
-
-    // $(".btn-talla-xs").click(function(){
-    //     // bgcolorxs = "var(--color-rosa-hover)";
-    //     $(this).css('background-color', 'var(--color-rosa-hover)');
-    //     $(this).css('color', 'var(--color-blanco-fondo)');
-    // })
-    // $(".btn-talla-s").click(function(){
-
-    //     $(this).css('background-color', 'var(--color-rosa-hover)');
-    //     $(this).css('color', 'var(--color-blanco-fondo)');
-    //     if (bgcolorxs === "var(--color-rosa-hover)") {
-    //         $('.btn-talla-xs').css("background-color") = "var(--color-blanco-fondo)";
-    //         console.log("hola");
-    //     }
-    // })
     // BOTONOES CANTIDAD
     $(document).ready(function(){
         var contador = 1;
